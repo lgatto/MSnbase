5469de477af10489ff77364754ab6a97fc570f9c
lgatto <laurent.gatto@gmail.com>
2011-02-15 20:39:05 +0000

    updated plot.Spectrum2, adding reporters colours on plot

--- R/plotting-Spectrum2.R
+++ R/plotting-Spectrum2.R
@@ -9,8 +9,28 @@ plot.Spectrum2 <- function(spectrum,reporters=NULL,full=FALSE) {
   mainvp <- viewport(width=1,height=1,x=0.5,y=0.5)
   title <- opts(title=paste("Precursor M/Z",spectrum@precursorMz))
   p <- ggplot(df,aes(x=mtc,y=i)) + 
-              labs(x="M/Z",y="Intensity (ion counts)") +
-              geom_line()
+              labs(x="M/Z",y="Intensity (ion counts)")
+  if (!is.null(reporters)) {
+    rect <- data.frame(mtc=mean(df$mtc),
+                       i=0,
+                       xmin=reporters@mz-reporters@width,
+                       xmax=reporters@mz+reporters@width,
+                       ymin=0,
+                       ymax=max(df$i),
+                       fill=reporters@col,
+                       alpha=1/3)
+    p <- p + geom_rect(data=rect,
+                       aes(xmin=xmin,
+                           xmax=xmax,
+                           ymin=ymin,
+                           ymax=ymax,
+                           fill=fill,
+                           alpha=alpha)) 
+  }
+  p <- p +
+    geom_point(alpha=I(1/10)) +
+      geom_line() +
+        opts(legend.position = "none")
   if (!is.null(reporters)) {
     if (class(reporters)!="ReporterIons")
       stop("Reporters must be of class 'ReporterIons'.")
@@ -19,17 +39,18 @@ plot.Spectrum2 <- function(spectrum,reporters=NULL,full=FALSE) {
     rlim2 <- max(reporters@mz)+width
     coord <- coord_cartesian(xlim=c(rlim1,rlim2))
     subvp <- viewport(width=2/3,height=1/3,x=.95,y=.92,just=c("right","top"))
-    reps <- p + coord + geom_point(alpha=I(1/10)) +
+    reps <- p + coord + 
       theme_gray(9) +
         labs(x = NULL, y = NULL) +
           opts(plot.margin = unit(c(1,1,0,0), "lines")) +
-            scale_x_continuous(breaks=seq(rlim1,rlim2,(rlim2-rlim1)/10))
+            scale_x_continuous(breaks=seq(rlim1,rlim2,(rlim2-rlim1)/10)) +
+              opts(legend.position = "none") 
   }
   if (full) {
     print(p+title,vp=mainvp)
-    invisible(p+title)
-    if (!is.null(reporters)) 
+     if (!is.null(reporters)) 
       print(reps,vp=subvp)
+    invisible(p+title)
   } else {
     print(reps+title,vp=mainvp)
     invisible(reps+title)
