66abf1f4dcad8712a3165084f7ff75d652153a6b
lgatto <laurent.gatto@gmail.com>
2011-02-15 14:14:51 +0000

    iTRAQ8/9 updated

--- R/iTRAQ8.R
+++ R/iTRAQ8.R
@@ -5,7 +5,7 @@ iTRAQ8 <- new("ReporterIons",
                 "iTRAQ8.113","iTRAQ8.114","iTRAQ8.115","iTRAQ8.116",
                 "iTRAQ8.117","iTRAQ8.118","iTRAQ8.119","iTRAQ8.121"),
               mz=c(113.13,114.13,115.13,116.13,
-                117.13,118.13,119.13,120.08),
+                117.13,118.13,119.13,121.13),
               col=c("yellow","blue","palegreen3","red",
                 "tomato","black","purple","grey"),
               width=0.05)
