7eea9a7d66e0586bdcf37649a3c69e56258b2963
lgatto <laurent.gatto@gmail.com>
2011-02-18 13:40:11 +0000

    updated reporters and man

--- R/iTRAQ4.R
+++ R/iTRAQ4.R
@@ -3,6 +3,6 @@ iTRAQ4 <- new("ReporterIons",
               name="iTRAQ4",
               reporterNames=c("iTRAQ4.114","iTRAQ4.115",
                 "iTRAQ4.116","iTRAQ4.117"),
-              mz=c(114.13,115.13,116.13,117.13),
+              mz=c(114.1,115.1,116.1,117.1),
               col=c("red","green","blue","yellow"),
               width=0.05)
--- R/iTRAQ5.R
+++ R/iTRAQ5.R
@@ -3,6 +3,6 @@ iTRAQ5 <- new("ReporterIons",
               name="iTRAQ4",
               reporterNames=c("iTRAQ5.114","iTRAQ5.115",
                 "iTRAQ5.116","iTRAQ5.117","iTRAQ5.145"),
-              mz=c(114.13,115.13,116.13,117.13,145.13),
+              mz=c(114.1,115.1,116.1,117.1,145.1),
               col=c("red","green","blue","yellow","grey"),
               width=0.05)
