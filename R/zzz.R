808c51eaac3f0b7027560853465d98f624f19190
Laurent Gatto <l.gatto@dnavision.be>
2011-06-26 18:40:31 +0000

    added plotMzDelta paragraph in demo vignette
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@56414 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,4 +3,7 @@
                         paste("\nThis is MSnbase version",packageVersion("MSnbase"),"\n",
                               " Read '?MSnbase' and references therein for information\n",
                               " about the package and how getting started.\n"))
+  addVigs2WinMenu("MSnbase-demo")
+  addVigs2WinMenu("MSnbase-dev")
 }
+
