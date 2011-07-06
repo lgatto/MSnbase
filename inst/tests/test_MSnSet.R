a3f1a3c551eb23889377a35cec5a0b7e7ad020b1
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-07-06 15:04:43 +0000

    new transpose and [ methods for MSnSet
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@56585 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/tests/test_MSnSet.R
+++ inst/tests/test_MSnSet.R
@@ -43,7 +43,7 @@ test_that("Purity correction", {
   expect_true(all(exprs(pc)==exprs(msnset)))
 })
 
-test_that("Normalisation", {
+test_that("Normalisation and transpose", {
   bb <- quantify(itraqdata,method="trap",reporters=iTRAQ4,verbose=FALSE)
   bb1 <- normalise(bb,"sum")
   expect_true(all.equal(rowSums(exprs(bb1),na.rm=TRUE),
@@ -53,4 +53,19 @@ test_that("Normalisation", {
   bb3 <- normalise(bb,"quantiles")
   bb4 <- normalise(bb,"quantiles.robust")
   bb5 <- normalise(bb,"vsn")
+})
+
+
+test_that("Transpose and subset", {
+  aa <- quantify(itraqdata,method="trap",reporters=iTRAQ4,verbose=FALSE)
+  ## transpose
+  expect_warning(taa <- t(aa),"Dropping protocolData.")
+  expect_true(nrow(aa)==ncol(taa)) 
+  expect_true(ncol(aa)==nrow(taa))
+  expect_true(all.equal(pData(aa),fData(taa)))
+  expect_true(all.equal(pData(taa),fData(aa)))
+  ## subset
+  bb <- aa[1:2,c(2,4)]
+  expect_true(all(dim(qual(bb))==c(4,7)))
+  expect_true(all(qual(bb)$reporter %in% bb$mz))
 })
