345b86e75ae9a8d54dce38ae6c4abed967f6be4c
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-09-21 18:21:29 +0000

    Fixed/improved [read|write]MgfData and getCurveWidth
    CHANGES IN VERSION 1.1.23
    -------------------------
     o fixed writeMgfData("MSnExp") <2011-09-21 Wed>
     o readMgfData now works when mz and intensty are separated 
       by a '\t' (as exported by PRIDE Inspector) <2011-09-21 Wed>
     o show("MSnExp") now works without retention time <2011-09-21 Wed>
     o updated mgf2Spectrum2 to make it faster <2011-09-21 Wed>
     o fixed missing fromFile slot in data created from readMgfData 
       that prevented calling header <2011-09-21 Wed>
     o readMgfData now creates a fData based on the peak header <2011-09-21 Wed>
     o modified getCurveWidth to work with centroided data <2011-09-21 Wed>
     o fixed bug getCurveWidth <2011-09-21 Wed>
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@58296 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readWriteMgfData.R
+++ R/readWriteMgfData.R
@@ -26,10 +26,10 @@ setMethod("writeMgfData",
           })
 
 writeMgfContent <- function(sp,TITLE=NULL,filename) {
-  cat("BEGIN IONS\n",file=filename,append=FALSE)  
+  cat("BEGIN IONS\n",file=filename,append=TRUE)  
   cat("SCANS=",acquisitionNum(sp),"\n",sep="",file=filename,append=TRUE)
   if (is.null(TITLE))
-    cat("TITLE=MS ",msLevel(sp)," spectrum\n",sep="",file=filename,append=TRUE)
+    cat("TITLE=MS",msLevel(sp),"spectrum\n",sep="",file=filename,append=TRUE)
   else 
     cat("TITLE=",TITLE,"\n",sep="",file=filename,append=TRUE)
   cat("RTINSECONDS=",rtime(sp),"\n",sep="",file=filename,append=TRUE)
@@ -49,72 +49,103 @@ readMgfData <- function(file,
                         centroided=TRUE,
                         smoothed=FALSE,
                         verbose=TRUE) {
-    mgf <- scan(file=file, what="", sep="\n", quote="", allowEscapes=FALSE, quiet=TRUE)
-    begin <- grep("BEGIN IONS", mgf)
-    end <- grep("END IONS", mgf)
+  if (verbose)
+    cat("Scanning ",file,"...\n",sep="")
+  mgf <- scan(file=file, what="", sep="\n", quote="", allowEscapes=FALSE, quiet=TRUE)
+  begin <- grep("BEGIN IONS", mgf)
+  end <- grep("END IONS", mgf)
+  if (verbose) {
+    cnt <- 1
+    pb <- txtProgressBar(min = 0, max = length(begin), style = 3)
+    
+  }
+  spectra <- vector("list",length=length(begin))
+  fdata <- c()
+  for (i in 1:length(begin)) {
     if (verbose) {
-      ._cnt <- 1
-      pb <- txtProgressBar(min = 0, max = length(begin), style = 3)
-
+      setTxtProgressBar(pb, cnt)
+      cnt <- cnt + 1
     }
-    spectra <- lapply(1:length(begin),
-                      function(i) {
-                        if (verbose) {
-                          setTxtProgressBar(pb, ._cnt)
-                          ._cnt <<- ._cnt+1
-                        }
-                        mgfToSpectrum2(mgf[seq(begin[i], end[i])],centroided=centroided)
-                      })
-    if (verbose) {
-      close(pb)
-      rm(pb)
-      rm(._cnt)
-    }
-    nms <- paste("X",seq_len(length(spectra)),sep="")
-    names(spectra) <- nms
-    spectra.env <- list2env(spectra)
-    process <- new("MSnProcess",
-                   processing=paste("Data loaded:",date()),
-                   files=file,
-                   smoothed=smoothed)
-    if (is.null(pdata)) {
-        pdata <- new("NAnnotatedDataFrame",
-                     dimLabels=c("sampleNames", "sampleColumns"))
-    }
-    fdata <- new("AnnotatedDataFrame",
-                 data=data.frame(spectrum=1:length(spectra),
-                 row.names=nms))
-    fdata <- fdata[ls(spectra.env)] ## reorder features
-    toReturn <- new("MSnExp",
-                    assayData = spectra.env,
-                    phenoData = pdata,
-                    featureData = fdata,
-                    processingData = process)
-    if (validObject(toReturn))
-        return(toReturn)
+    spec <- mgf[seq(begin[i], end[i])]
+    spectra[[i]] <- mgfToSpectrum2(spec,centroided=centroided)
+    ## adding the spectrum header as fData
+    tmpdesc <- spec[grep("=",spec)]
+    tmpdesc <- sapply(tmpdesc,strsplit,"=")
+    desc <- unlist(lapply(tmpdesc,"[",2))
+    names(desc) <- unlist(lapply(tmpdesc,"[",1))
+    fdata <- rbind(fdata,desc)
+  }
+  if (verbose) 
+    close(pb)
+  nms <- paste("X",seq_len(length(spectra)),sep="")
+  names(spectra) <- nms
+  spectra.env <- list2env(spectra)
+  process <- new("MSnProcess",
+                 processing=paste("Data loaded:",date()),
+                 files=file,
+                 smoothed=smoothed)
+  if (is.null(pdata)) {
+    pdata <- new("NAnnotatedDataFrame",
+                 dimLabels=c("sampleNames", "sampleColumns"))
+  }
+  ## fdata <- new("AnnotatedDataFrame",
+  ##              data=data.frame(spectrum=1:length(spectra),
+  ##                row.names=nms))
+  colnames(fdata) <- names(desc)
+  rownames(fdata) <- nms
+  fdata <- AnnotatedDataFrame(data=data.frame(fdata))
+  fdata <- fdata[ls(spectra.env)] ## reorder features
+  toReturn <- new("MSnExp",
+                  assayData = spectra.env,
+                  phenoData = pdata,
+                  featureData = fdata,
+                  processingData = process)
+  if (validObject(toReturn))
+    return(toReturn)
 }
 
 
 mgfToSpectrum2 <- function(mgf, centroided) {
     mgf <- mgf[c(-1, -length(mgf))] ## remove "BEGIN IONS" and "END IONS"
+    mgf <- sub("\t"," ",mgf) ## expecting mz and int to be separated by 1 space
     desc.idx <- grep("=",mgf)
-    mz.i <- adply(.data=mgf[-desc.idx], .margins=1, .fun=function(i) unlist(strsplit(i, split=" ")))
-    mz <- as.numeric(mz.i$V1)
-    int <- as.numeric(mz.i$V2)
-    desc <- adply(.data=mgf[desc.idx], .margins=1, .fun=function(i) unlist(strsplit(i, split="=")))
-    desc <- desc[,-1]
+    desc <- mgf[desc.idx]
+    spec <- mgf[-desc.idx]
+    ## mz.i <- adply(.data=mgf[-desc.idx], .margins=1, .fun=function(i) unlist(strsplit(i, split=" ")))
+    ## mz <- as.numeric(mz.i$V1)
+    ## int <- as.numeric(mz.i$V2)
+    mz.i <- sapply(spec,strsplit," ")
+    mz <- as.numeric(unlist(lapply(mz.i,"[",1)))
+    int <- as.numeric(unlist(lapply(mz.i,"[",2)))
+    ## desc <- adply(.data=mgf[desc.idx], .margins=1, .fun=function(i) unlist(strsplit(i, split="=")))
+    ## desc <- desc[,-1]
+    tmpdesc <- sapply(desc,strsplit,"=")
+    desc <- unlist(lapply(tmpdesc,"[",2))
+    names(desc) <- unlist(lapply(tmpdesc,"[",1))
+    ## sp <- new("Spectrum2",
+    ##           rt=as.numeric(desc[desc[,1] == "RTINSECONDS",2]),
+    ##           acquisitionNum=as.integer(desc[desc[,1] == "SCANS",2]),
+    ##           precursorMz=as.numeric(desc[desc[,1] == "PEPMASS",2]),
+    ##           #precursorIntensity="",  ##I don't see any definition in MGF.
+    ##           precursorCharge=as.integer(sub("[+,-]","",desc[desc[,1] == "CHARGE",2])),
+    ##           #scanIndex="",
+    ##           #collisionEnergy="",
+    ##           peaksCount=length(int),
+    ##           mz=mz,
+    ##           intensity=int,
+    ##           fromFile=1L,
+    ##           centroided=centroided)
     sp <- new("Spectrum2",
-              rt=as.numeric(desc[desc[,1] == "RTINSECONDS",2]),
-              acquisitionNum=as.integer(desc[desc[,1] == "SCANS",2]),
-              precursorMz=as.numeric(desc[desc[,1] == "PEPMASS",2]),
-              #precursorIntensity="",  ##I don't see any definition in MGF.
-              precursorCharge=as.integer(sub("[+,-]","",desc[desc[,1] == "CHARGE",2])),
-              #scanIndex="",
-              #collisionEnergy="",
-              peaksCount=length(int),
-              mz=mz,
-              intensity=int,
-              centroided=centroided)
+              rt = ifelse("RTINSECONDS" %in% names(desc),as.numeric(desc["RTINSECONDS"]),0),
+              acquisitionNum = ifelse("SCANS" %in% names(desc),as.integer(desc["SCANS"]),0L),
+              precursorMz = ifelse("PEPMASS" %in% names(desc),as.numeric(desc["PEPMASS"]),0L),
+              precursorCharge = ifelse("CHARGE" %in% names(desc),as.integer(sub("[+,-]","",desc["CHARGE"])),0L),
+              peaksCount = length(int),
+              mz = mz,
+              intensity = int,
+              fromFile = 1L,
+              centroided = centroided)
+
     if(validObject(sp))
         return(sp)
 }
