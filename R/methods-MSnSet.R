13edf2324eededb3e704178e60e9d836bf45ba32
Laurent Gatto <l.gatto@dnavision.be>
2012-09-13 14:45:28 +0000

    new MAplot method for MSnSet instances, using affy generic and functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@69409 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-MSnSet.R
+++ R/methods-MSnSet.R
@@ -394,3 +394,26 @@ setMethod("log",
             if (validObject(x))
               return(x)
           })
+
+
+setMethod("MAplot",
+          signature = "MSnSet",
+          function(object, log.it = TRUE, base = 2, ...) {
+            if (ncol(object) < 2)
+              stop("Need at least 2 samples to produce an MA plot.")
+            if (log.it)
+              object <- log(object, base)              
+            if (ncol(object) == 2) {
+              x <- exprs(object[, 1])
+              y <- exprs(object[, 2])
+              sel <- !is.na(x) & !is.na(y)
+              x <- x[sel]
+              y <- y[sel]
+              M <- x - y
+              A <- (x + y)/2
+              o <- order(M)
+              ma.plot(A[o], M[o], ...)
+            } else {
+              mva.pairs(exprs(object), log.it = FALSE, ...)
+            }
+          })
