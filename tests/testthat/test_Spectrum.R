406b5e9bc6d1c418f2b6ec1ee454548f2dcaac12
Sebastian Gibb <mail@sebastiangibb.de>
2014-04-10 21:56:27 +0100

    add simple test cases for smooth and pickPeaks

--- tests/testthat/test_Spectrum.R
+++ tests/testthat/test_Spectrum.R
@@ -36,7 +36,7 @@ test_that("Spectrum processing", {
   expect_that(ionCount(sp4),equals(sum(int[10:20])))  
 })
 
-test_that("Spectrum processing", {
+test_that("Spectrum normalisation", {
   s1 <- new("Spectrum1", mz=1:5, intensity=1:5)
   s2 <- new("Spectrum2", mz=1:5, intensity=1:5, precursorIntensity=10)
 
@@ -57,6 +57,23 @@ test_that("Spectrum processing", {
   expect_equal(intensity(normalize(s2, method="precursor")), (1:5)/10)
   expect_equal(intensity(normalize(s2, method="precursor", 
                          precursorIntensity=20)), (1:5)/20)
+})
+
+test_that("Peak picking", {
+  s1 <- new("Spectrum2", mz=1:5, intensity=c(1:3, 2:1))
+  s2 <- new("Spectrum2", mz=3, intensity=3, centroided=TRUE)
+
+  expect_warning(pickPeaks(new("Spectrum2")), "spectrum is empty")
+  expect_warning(pickPeaks(s2), "spectrum is already centroided")
+  expect_equal(pickPeaks(s1), s2)
+})
+
+test_that("Spectrum smoothing", {
+  s1 <- new("Spectrum2", mz=1:5, intensity=c(1:3, 2:1))
+  s2 <- new("Spectrum2", mz=1:5, intensity=c(2, 2, 2+1/3, 2, 2))
+
+  expect_warning(smooth(new("Spectrum2")), "spectrum is empty")
+  expect_equal(smooth(s1, method="MovingAverag", halfWindowSize=1), s2)
 })
 
 test_that("Spectrum quantification", {
