dd651ba8a2b3794c8887c5f90fd8c93c4e9edc36
Laurent <lg390@cam.ac.uk>
2014-11-20 19:22:34 +0000

    imageNA2 with example

--- R/imageNA.R
+++ R/imageNA.R
@@ -1,3 +1,6 @@
+##' Produces a heatmap after reordring rows and columsn to highlight
+##' missing value patterns.
+##'
 ##' @title NA heatmap visualisation for 2 groups
 ##' @param object An instance of class MSnSet
 ##' @param pcol Either the name of a phenoData variable to be used to
@@ -14,33 +17,56 @@
 ##' are reordered. Default is \code{TRUE}.
 ##' @param useGroupMean Replace individual feature intensities by the
 ##' group mean intensity. Default is FALSE.
+##' @param plot A \code{logical} specifying of an image should be
+##' produced. Default is \code{TRUE}.
 ##' @param ... Additional arguments passed to \code{\link{image}}.
-##' @return Used for its side effect of plotting
-##' @author Laurent Gatto
-.imageNA2 <- function(object, pcol,
-                      Rowv, Colv = TRUE,
-                      useGroupMean = FALSE, ...) {   
-    if (missing(pcol))
+##' @return Used for its side effect of plotting. Invisibly returns
+##' Rovw and Colv.
+##' @author Laurent Gatto, Samuel Wieczorek and Thomas Burger
+##' @examples
+##' library("pRolocdata")
+##' library("pRoloc")
+##' data(dunkley2006)
+##' pcol <- ifelse(dunkley2006$fraction <= 5, "A", "B")
+##' nax <- makeNaData(dunkley2006, pNA = 0.10)
+##' exprs(nax)[sample(nrow(nax), 30), pcol == "A"] <- NA
+##' exprs(nax)[sample(nrow(nax), 50), pcol == "B"] <- NA
+##' imageNA2(nax, pcol)
+##' imageNA2(nax, pcol, useGroupMean = TRUE)
+##' imageNA2(nax, pcol, Colv = FALSE, useGroupMean = FALSE)
+##' imageNA2(nax, pcol, Colv = FALSE, useGroupMean = TRUE)
+imageNA2 <- function(object, pcol,
+                     Rowv, Colv = TRUE,
+                     useGroupMean = FALSE,
+                     plot = TRUE,
+                     ...) {
+    if (missing(pcol)) {
         image(object)
-    if (is.character(pcol)) {
-        if (pcol %in% varLabels(phenoData(object)))
+        return(invisible(NULL))
+    }
+    if (is.character(pcol) & length(pcol) == 1) {
+        if (pcol %in% varLabels(phenoData(object))) {
             pcol <- factor(pData(object)[, pcol])
+        }
         else
             stop(pcol, " not found in varLabels(phenoData(object)): ",
                  paste(varLabels(phenoData(object)), collapse = " "))
     }
     pcol <- as.factor(pcol)
     stopifnot(length(levels(pcol)) == 2)
+    po <- order(pcol)
+    pcol <- pcol[po]
+    object <- object[, po]    
     g1 <- pcol == levels(pcol)[1]
     g2 <- pcol == levels(pcol)[2]        
     if (missing(Rowv)) {
         nNA1 <- apply(exprs(object), 1, function(x) sum(is.na(x[g1])))
-        nNA2 <- apply(exprs(xx), 1, function(x) sum(is.na(x[g2])))
+        nNA2 <- apply(exprs(object), 1, function(x) sum(is.na(x[g2])))
         Rowv <- order((nNA1 + 1)^2/(nNA2 + 1))
     }
     fn0 <- featureNames(object)
     object <- object[Rowv, ]
-    if (identical(sort(fn0), featureNames(object)))
+    if (!identical(sort(fn0), sort(featureNames(object))))
         warning("Feature names are different before and after reordering.")
     if (Colv) {
         ## reordering each protein values individually
@@ -56,13 +82,12 @@
             k1 <- exprs(object)[i, g1]        
             m1 <- mean(k1, na.rm=TRUE)    
             k1[!is.na(k1)] <- m1    
-            k1[rev(which(g1))] <- k1[order(k1)]
             k2 <- exprs(object)[i, g2]       
             m1 <- mean(k2, na.rm=TRUE)    
             k2[!is.na(k2)] <- m1    
-            k2 <- k2[order(k2)]
             exprs(object)[i, ] <- c(k1, k2)
         }        
     }
-    image(object, ...)    
+    image(object, ...)
+    invisible(list(Rowv = Rowv, Colv = Colv))
 }
