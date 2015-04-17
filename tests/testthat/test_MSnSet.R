9f3aff26e7d35ea1f5e1277f16c25d05315d1116
Laurent <lg390@cam.ac.uk>
2015-04-17 16:45:39 +0100

    new commonFeatureNames function

--- tests/testthat/test_MSnSet.R
+++ tests/testthat/test_MSnSet.R
@@ -279,3 +279,26 @@ test_that("idSummary", {
                           stringsAsFactors=FALSE))
 })
 
+test_that("keeping common features", {
+              library("pRolocdata")
+              data(tan2009r1)
+              data(tan2009r2)
+              data(tan2009r3)
+              res0 <- commonFeatureNames(tan2009r1, tan2009r1)
+              ## @qual and @processingData@processing will be different
+              expect_equal(res0[[1]], res0[[2]])
+              res0[[1]]@qual <- tan2009r1@qual
+              res0[[1]] <- MSnbase:::nologging(res0[[1]])
+              expect_equal(res0[[1]], tan2009r1)
+              res1 <- commonFeatureNames(tan2009r1, tan2009r2)
+              res2 <- commonFeatureNames(list(tan2009r1, tan2009r2))
+              res3 <- commonFeatureNames(list(tan2009r1 = tan2009r1,
+                                              tan2009r2 = tan2009r2))
+              ## the only expected difference are
+              ## names and .@processingData@processing
+              res1 <- lapply(res1, MSnbase:::nologging)
+              res2 <- lapply(res2, MSnbase:::nologging)
+              expect_equal(res1, res2, check.attributes = FALSE)
+              expect_null(names(res2))
+              expect_equal(names(res1), names(res3))
+})
