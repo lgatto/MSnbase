1c1eac13c0cb1a096dd2e7c040bf651577e8df53
Laurent Gatto <l.gatto@dnavision.be>
2015-06-25 18:50:41 +0000

    Merge branch 'master' into local-hedgehog
    From: Laurent <lg390@cam.ac.uk>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@105501 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-fragments.R
+++ R/methods-fragments.R
@@ -1,6 +1,7 @@
 setMethod("calculateFragments", c("character", "missing"),
           function(sequence, type=c("b", "y"), z=1,
-                   modifications=c(C=57.02146), neutralLoss=TRUE,
+                   modifications=c(C=57.02146),
+                   neutralLoss=defaultNeutralLoss(),
                    verbose=TRUE) {
             l <- lapply(sequence, .calculateFragments,
                         type=type, z=z, modifications=modifications,
