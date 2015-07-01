648d376019622293a6fc2dcfe64d524bf70602e7
Laurent Gatto <l.gatto@dnavision.be>
2015-07-01 22:17:49 +0000

    Merge branch 'master' into local-hedgehog
    From: Laurent <lg390@cam.ac.uk>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@105756 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test_MzTab.R
@@ -0,0 +1,53 @@
+context("MSzTab class")
+
+test_that("MzTab creation and accessors", {
+              fl <- "iTRAQ_CQI.mzTab"
+              baseUrl <- "https://mztab.googlecode.com/svn/examples/"
+              fl <- file.path(baseUrl, fl)
+              xx <- MzTab(fl)
+              expect_true(validObject(xx))
+              expect_null(show(xx))
+              ## Accessors
+              expect_is(metadata(xx), "list")
+              expect_identical(length(metadata(xx)), 64L)
+              expect_identical(mzTabMode(xx), metadata(xx)$`mzTab-mode`)
+              expect_identical(mzTabMode(xx), "Complete")
+              expect_identical(mzTabType(xx), metadata(xx)$`mzTab-type`)
+              expect_identical(mzTabType(xx), "Quantification")
+              expect_identical(fileName(xx), fl)
+              expect_is(proteins(xx), "data.frame")
+              expect_identical(dim(proteins(xx)), c(5L, 55L))
+                            expect_is(peptides(xx), "data.frame")
+              expect_identical(dim(peptides(xx)), c(0L, 0L))
+              expect_is(psms(xx), "data.frame")
+              expect_identical(dim(psms(xx)), c(36L, 18L))
+              expect_is(smallMolecules(xx), "data.frame")
+              expect_identical(dim(smallMolecules(xx)), c(0L, 0L))
+              expect_is(comments(xx), "character")
+              expect_identical(length(comments(xx)), 5L)
+})
+
+test_that("Conversion to MSnSetList", {
+              fl <- "iTRAQ_CQI.mzTab"
+              baseUrl <- "https://mztab.googlecode.com/svn/examples/"
+              fl <- file.path(baseUrl, fl)
+              xx <- MzTab(fl)
+              msl <- as(xx, "MSnSetList")
+              expect_true(validObject(msl))
+              expect_null(show(msl))
+              expect_true(length(msl) == 3L)
+              dims0 <- structure(list(Proteins = c(5L, 16L),
+                                      Peptides = c(0L, 0L),
+                                      PSMs = c(36L, 0L)))
+              expect_identical(lapply(msl, dim), dims0)
+              ##
+              ms <- msl[['Proteins']]
+              dfr <- proteins(xx)
+              expect_identical(ncol(dfr), length(fvarLabels(ms)) + ncol(ms))
+              ms <- msl[['Peptides']]
+              dfr <- peptides(xx)
+              expect_identical(ncol(dfr), length(fvarLabels(ms)) + ncol(ms))
+              ms <- msl[['PSMs']]
+              dfr <- psms(xx)
+              expect_identical(ncol(dfr), length(fvarLabels(ms)) + ncol(ms))              
+})
--- tests/testthat/test_foi.R
+++ tests/testthat/test_foi.R
@@ -3,18 +3,43 @@ context("Features of interest")
 test_that("FeaturesOfInterest", {
     library("pRolocdata")
     data(tan2009r1)
+
+    fn <- featureNames(tan2009r1)[1:3]
+    foi0 <- FeaturesOfInterest(fn, "small foi")
+    expect_null(show(foi0))
+    expect_equal(length(foi0@objpar), 0L) ## not traceable
+    
     fn <- featureNames(tan2009r1)[1:10]
     desc <- "my description"
     foi1 <- FeaturesOfInterest(fn, desc, tan2009r1)
+    expect_null(show(foi1))
+    expect_equal(length(foi1@objpar), 4L) ## traceable
+    expect_identical(description(foi1), desc)
     foi2 <- FeaturesOfInterest(fn, desc)
     
     expect_equal(length(foi1), length(fn))
     expect_equal(length(foi2), length(fn))
 
-    col <- FoICollection()
+    col <- FoICollection()    
     expect_equal(length(col), 0)
     col <- addFeaturesOfInterest(foi1, col)
     expect_equal(length(col), 1)
+    expect_null(show(col))
+
+    expect_message(col1 <- addFeaturesOfInterest(foi2, col),
+                   "The features of interest are already present.")
+    expect_identical(col, col1)
+    
+    col2 <- FoICollection(list(foi1))
+    expect_identical(col, col2)
+    expect_identical(lengths(col), 10L)
+
+    col3 <- FoICollection(list(foi1, foi2))
+    expect_identical(foi(col3, 1), list(foi1))
+    expect_error(foi(col3, 3),
+                 "There are only 2 available FeatureOfInterest instances.")
+    
+    expect_identical(description(col3), rep(desc, 2))
     
     expect_message(col0 <- addFeaturesOfInterest(foi1, col),
                    "The features of interest are already present.")
@@ -23,7 +48,24 @@ test_that("FeaturesOfInterest", {
     expect_message(col0 <- addFeaturesOfInterest(foi2, col),
                    "The features of interest are already present.")
     expect_true(all.equal(col, col0))
+
+    expect_identical(rmFeaturesOfInterest(col3, 2), col2)
+
+
+    foil <- lapply(replicate(10, sample(nrow(tan2009r1), 15), simplify=FALSE),
+                   function(i) FeaturesOfInterest(featureNames(tan2009r1)[i], "my fois"))
+    col4 <- FoICollection(foil)
+    expect_identical(lengths(col4), rep(15L, 10))
+    expect_null(show(col4))
+
+    foim <- as(col4, "matrix")
+    expect_identical(foim, as.matrix.FoICollection(col4))
+    expect_identical(length(col4), ncol(foim))
+
+    ufns <- unique(unlist(lapply(foi(col4), foi)))
+    expect_identical(ufns, rownames(foim))                     
 })
+
 
 
 test_that("FeaturesOfInterest traceable and fnamesIn", {
@@ -35,6 +77,11 @@ test_that("FeaturesOfInterest traceable and fnamesIn", {
     f <- FeaturesOfInterest(c("AA", fn), desc)
     expect_equal(length(f), 11)
     expect_true(fnamesIn(f, tan2009r1))
+    expect_true(fnamesIn(f, exprs(tan2009r1)))
+    tandfr <- as(t(tan2009r1), "data.frame")
+    expect_true(fnamesIn(f, tandfr))
+    
     expect_equal(fnamesIn(f, tan2009r1, count = TRUE), 10)
     expect_error(FeaturesOfInterest(letters[1:5], desc, tan2009r1))    
 })
+
