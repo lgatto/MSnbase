73fc3711b6e327ffd9324efb113f59863d472f85
Laurent <lg390@cam.ac.uk>
2015-07-26 23:01:14 +0100

    fix unit test for image output

--- tests/testthat/test_plotting.R
+++ tests/testthat/test_plotting.R
@@ -2,11 +2,8 @@ context("Plotting functions")
 
 test_that("image,MSnSet-method", {
               data(naset)
-              ## only difference between these two plots is that
-              ## the former uses Samples/Features as xlab/ylab 
-              ## and the latter Columns/Samples
               par(mfrow = c(1, 2))
-              expect_null(image(naset))
+              expect_is(image(naset), "gg")
               expect_null(image2(exprs(naset)))
               dev.off()
           })
