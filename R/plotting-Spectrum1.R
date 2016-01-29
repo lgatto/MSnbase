928ab1875e5ce6846c1f6951787b3c4d4d021581
Laurent Gatto <l.gatto@dnavision.be>
2016-01-29 12:10:56 +0000

    out of date files and conflicts
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@113062 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotting-Spectrum1.R
+++ R/plotting-Spectrum1.R
@@ -7,16 +7,16 @@ plot_Spectrum1 <- function(spectrum,
   if (centroided.) {
     if (missing(w1))
       w1 <- max(mtc)/500
-    dfr <- data.frame(i=i,mtc=mtc,width=w1)      
-    p <- ggplot(dfr,aes(x=mtc,y=i,width=width)) + 
-      geom_bar(stat="identity",position="identity")
+    dfr <- data.frame(i = i, mtc = mtc, width = w1)
+    p <- ggplot(dfr, aes(x = mtc, y = i, width = width)) +
+      geom_bar(stat="identity", position="identity")
   } else {
-    dfr <- data.frame(i=i,mtc=mtc)
-    p <- ggplot(dfr,aes(x=mtc,y=i)) + geom_line()
+    dfr <- data.frame(i = i, mtc = mtc)
+    p <- ggplot(dfr, aes(x = mtc, y = i)) + geom_line()
   }
   title <- ggtitle(paste("Retention time", rtime(spectrum)))
-  p <- p + labs(x="M/Z",y="Intensity") + title
+  p <- p + labs(x="M/Z", y="Intensity") + title
   if (plot)
-    print(p+title)
-  invisible(p+title)
+    print(p + title)
+  invisible(p + title)
 }
