83c5fc8e176c5d2f653990d5726dbc8cb3194418
Laurent <lg390@cam.ac.uk>
2016-04-21 08:23:07 +0100

    nadata test stub

--- tests/testthat/test_nadata.R
+++ tests/testthat/test_nadata.R
@@ -1,6 +1,13 @@
 context("NA data")
 
 test_that("makeNAdata", {
-    expect_error(makeNAdata)
+    expect_error(makeNAdata(1))
+})
 
+
+test_that("makeNAdata2", {
+    expect_error(makeNAdata2(1))
+})
+
+test_that("whichNA", {
 })
