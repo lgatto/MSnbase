eb4959e40e3e58e1cdbc68d0a579eb0f8c20fb0e
Laurent <lg390@cam.ac.uk>
2016-04-24 08:43:18 +0100

    more unit testing

--- tests/testthat/test_MIAPE.R
+++ tests/testthat/test_MIAPE.R
@@ -2,5 +2,7 @@ context("MIAPE class")
 
 test_that("MIAPE validity", {
     expect_true(validObject(new("MIAPE")))
+    data(itraqdata)
+    expect_null(show(experimentData(itraqdata)))
+    expect_null(msInfo(itraqdata))
 })
-
