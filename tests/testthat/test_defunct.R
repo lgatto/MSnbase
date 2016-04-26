d8d07d07f42ff63c8017c02480937fb61b4e1e7c
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-04-26 18:19:13 +0000

    Merge branch 'master' into devel
    * master:
      update news
      few more MIAPE funs coversage
      update news
      more unit testing
      Revert "revert error catching in utils.mergeSpectraAndIdentificationData"
      record removing readIspyData functions in news
      remove readIspyData functions
      add missing space
      revert error catching in utils.mergeSpectraAndIdentificationData
      fix error catching in utils.mergeSpectraAndIdentificationData
      update news
      more unit tests
      new gh devel version
    
    From: Laurent <lg390@cam.ac.uk>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@116800 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test_defunct.R
@@ -0,0 +1,7 @@
+test_that("All defunct functions", {
+    expect_error(.Defunct(readMzXMLData()), "Defunct")
+    expect_error(.Defunct(readMzXMLData()), "Defunct")
+    expect_error(.Defunct(makeMTD()), "Defunct")
+    expect_error(.Defunct(makePRT()), "Defunct")
+    expect_error(.Defunct(makePEP()), "Defunct")    
+})
