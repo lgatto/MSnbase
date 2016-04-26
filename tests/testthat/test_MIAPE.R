d878fe8cf42ea0af8006abdc22fff0ab6b2868c4
Laurent <lg390@cam.ac.uk>
2016-04-26 16:29:43 +0100

    few more MIAPE funs coversage

--- tests/testthat/test_MIAPE.R
+++ tests/testthat/test_MIAPE.R
@@ -4,5 +4,8 @@ test_that("MIAPE validity", {
     expect_true(validObject(new("MIAPE")))
     data(itraqdata)
     expect_null(show(experimentData(itraqdata)))
-    expect_null(msInfo(itraqdata))
+    itraqdata@experimentData@instrumentModel <- "test"
+    itraqdata@experimentData@other <- list(Note1 = "First note")
+    expect_null(show(experimentData(itraqdata)))
+    expect_null(msInfo(itraqdata))        
 })
