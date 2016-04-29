2d1f559282d57646baa56b9b9205f7a4e42b5f2a
Laurent Gatto <l.gatto@dnavision.be>
2016-04-29 07:55:58 +0000

    Merge branch 'master' into devel
    * master:
      chormatogram method unit test
      more mzR funs unit tests
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@116913 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_mzR-funs.R
+++ tests/testthat/test_mzR-funs.R
@@ -25,3 +25,31 @@ test_that("peaksAsList function", {
     expect_identical(pli, pl[1])
     expect_identical(plii, pl[1:2])
 })
+
+test_that(".chomatogram function", {
+    f <- dir(system.file("extdata", package = "MSnbase"),
+             full.names = TRUE, pattern = "mzXML")
+    ms <- openMSfile(f)
+    hd <- header(ms)
+    x <- MSnbase:::.chromatogram(hd, plot = FALSE)
+    expect_is(x, "data.frame")
+    expect_identical(nrow(x), length(ms))
+    expect_identical(colnames(x), c("rt", "tic"))
+    expect_identical(x$rt, hd$retentionTime)
+    expect_equal(x$tic, 100 * hd$totIonCurrent/ max(hd$totIonCurrent))    
+})
+
+
+test_that("chomatogram methods", {
+    library("msdata")
+    f <- dir(system.file("microtofq", package = "msdata"),
+             full.names = TRUE, pattern = "MM8.mzML")
+    library("mzR")
+    ms <- openMSfile(f)
+    ch1 <- chromatogram(f, plot = FALSE)
+    ch2 <- chromatogram(ms, plot = FALSE)
+    expect_identical(ch1, ch2)
+    hd <- header(ms)
+    ch3 <- MSnbase:::.chromatogram(hd, plot = FALSE)
+    expect_identical(ch1, ch3)
+})
