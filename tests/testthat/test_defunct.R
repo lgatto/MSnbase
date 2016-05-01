fe4cc8b6082ccf0e213f4cedb75eb0bcd25ce9f8
Laurent <lg390@cam.ac.uk>
2016-05-01 07:32:22 +0100

    add defunct test

--- tests/testthat/test_defunct.R
+++ tests/testthat/test_defunct.R
@@ -3,5 +3,6 @@ test_that("All defunct functions", {
     expect_error(.Defunct(readMzXMLData()), "Defunct")
     expect_error(.Defunct(makeMTD()), "Defunct")
     expect_error(.Defunct(makePRT()), "Defunct")
-    expect_error(.Defunct(makePEP()), "Defunct")    
+    expect_error(.Defunct(makePEP()), "Defunct")
+    expect_error(.Defunct(writeMzTabData()), "Defunct")
 })
