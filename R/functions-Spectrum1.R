6344b356903713736124d8f51757817c0f38782f
Laurent <lg390@cam.ac.uk>
2016-05-08 19:29:03 +0100

    More MzTab and Spectrum1 unit testing

--- R/functions-Spectrum1.R
+++ R/functions-Spectrum1.R
@@ -1,7 +1,7 @@
 show_Spectrum1 <- function(spectrum) {
-  cat("Object of class \"",class(spectrum),"\"\n",sep="")
-  cat(" Retention time:",formatRt(spectrum@rt),"\n")
-  cat(" MSn level:",spectrum@msLevel,"\n")
-  cat(" Total ion count:",spectrum@peaksCount,"\n")
-  cat(" Polarity:",spectrum@polarity,"\n")
+  cat("Object of class \"", class(spectrum), "\"\n", sep="")
+  cat(" Retention time:", formatRt(spectrum@rt), "\n")
+  cat(" MSn level:", spectrum@msLevel, "\n")
+  cat(" Total ion count:", spectrum@peaksCount, "\n")
+  cat(" Polarity:", spectrum@polarity, "\n")
 }
