4f228f984cccc860c180b327e17b94894ad7a49f
Laurent <lg390@cam.ac.uk>
2016-07-04 00:26:59 +0100

    filter functions stubs and export

--- R/methods-filters.R
+++ R/methods-filters.R
@@ -6,3 +6,27 @@ setMethod("filterMsLevel", "MSnExp",
               if (missing(msLevel.)) return(object)
               else object[msLevel(object) %in% msLevel.]
           })
+
+
+
+setMethod("filterMz", "MSnExp",
+          function(object, mz, msLevel.) {
+              ## TODO
+          })
+
+setMethod("filterRt", "MSnExp",
+          function(object, rt, msLevel.) {
+              ## TODO
+          })
+
+setMethod("filterFile", "MSnExp",
+          function(object, file) {
+              ## file can be a character or file index
+              ## TODO
+          })
+
+setMethod("filterAcquisitionNum", "MSnExp",
+          function(object, n) {
+              ## TODO
+          })
+
