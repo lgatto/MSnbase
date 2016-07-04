df2f2b8b60278450468557c119c38d92d7c802c7
Laurent <lg390@cam.ac.uk>
2016-07-04 23:31:10 +0100

    filterRt method

--- R/methods-filters.R
+++ R/methods-filters.R
@@ -3,8 +3,9 @@
 
 setMethod("filterMsLevel", "MSnExp",
           function(object, msLevel.) {
-              if (!missing(msLevel.))
-                  object <- object[msLevel(object) %in% msLevel.]
+              if (missing(msLevel.)) return(object)
+              msLevel. <- as.numeric(msLevel.)
+              object <- object[msLevel(object) %in% msLevel.]
               object <- logging(object,
                                 paste("Filter: select MS level(s)",
                                       paste(unique(msLevel.),
@@ -12,15 +13,38 @@ setMethod("filterMsLevel", "MSnExp",
               object
           })
 
+
+setMethod("filterRt", "MSnExp",
+          function(object, rt, msLevel.) {
+              if (missing(rt)) return(object)
+              if (length(rt) != 2 | !is.numeric(rt))
+                  stop("'rt' must be a numeric of length 2")
+              if (missing(msLevel.))
+                  msLevel. <- sort(unique(msLevel(object)))
+              msLevel. <- as.numeric(msLevel.)
+              ## MS levels that need to be filtered out
+              selms <- msLevel(object) %in% msLevel.
+              ## keep spectra that match MS levels and that are within
+              ## the retention time limits
+              sel1 <- rtime(object) >= rt[1] & rtime(object) <= rt[2] & selms
+              ## keep all spectra that don't match MS levels
+              sel2 <- !selms
+              object <- object[sel1 | sel2]
+              fltmsg <- paste0("Filter: select retention time [",
+                               paste0(rt, collapse = "-"),
+                               "] and MS level(s), ",
+                               paste(unique(msLevel.),
+                                     collapse = " "))
+              object <- logging(object, fltmsg)
+              object
+          })
+
+
 setMethod("filterMz", "MSnExp",
           function(object, mz, msLevel.) {
               ## TODO
           })
 
-setMethod("filterRt", "MSnExp",
-          function(object, rt, msLevel.) {
-              ## TODO
-          })
 
 setMethod("filterFile", "MSnExp",
           function(object, file) {
--- tests/testthat/test_MSnExpFilters.R
+++ tests/testthat/test_MSnExpFilters.R
@@ -1,25 +1,36 @@
 context("MSnExp filter functions")
 
 f <- msdata::proteomics(full.names = TRUE, pattern = "TMT_Erwinia_1")
-inmem <- readMSData(f, verbose = FALSE)  ## That's the MS 2 data.
+inmem2 <- readMSData(f, verbose = FALSE)  ## That's the MS 2 data.
 inmem1 <- readMSData(f, verbose = FALSE, msLevel = 1)  ## MS 1 data.
 ondisk <- readMSData2(f, verbose = FALSE)
+ondisk1 <- readMSData2(f, msLevel = 1, verbose = FALSE)
 ondisk2 <- readMSData2(f, msLevel = 2, verbose = FALSE)
 
 test_that("filterMsLevel", {
-    expect_true(all.equal(inmem, filterMsLevel(inmem, msLevel. = 2)))
+    expect_true(all.equal(inmem2, filterMsLevel(inmem2, msLevel. = 2)))
     expect_true(all.equal(ondisk2, filterMsLevel(ondisk2, msLevel. = 2)))
     ## Compare in-mem MS2 with filterMsLevel of on-disk MS 1 and 2.
-    expect_true(all.equal(inmem, filterMsLevel(ondisk, msLevel. = 2)))
+    expect_true(all.equal(inmem2, filterMsLevel(ondisk, msLevel. = 2)))
     ## Compare in-mem MS1 with filterMsLevel of on-disk MS 1 and 2
     expect_true(all.equal(inmem1, filterMsLevel(ondisk, msLevel. = 1)))
     ## in-mem MS 2 with on-disk MS 2
-    expect_true(all.equal(inmem, ondisk2))
-    expect_true(all.equal(filterMsLevel(inmem, msLevel. = 2),
+    expect_true(all.equal(inmem2, ondisk2))
+    expect_true(all.equal(filterMsLevel(inmem2, msLevel. = 2),
                           filterMsLevel(ondisk2, msLevel. = 2)))
-    expect_identical(length(filterMsLevel(inmem, 1)), 0L)
+    expect_identical(length(filterMsLevel(inmem2, 1)), 0L)
     expect_identical(length(filterMsLevel(ondisk2, 1)), 0L)
     expect_true(all.equal(filterMsLevel(ondisk, 2), ondisk2))
     expect_identical(length(filterMsLevel(ondisk, 1)),
                      sum(fData(ondisk)$msLevel == 1))
+})
+
+test_that("filterRt", {
+    rtlim <- c(122, 1300)
+    expect_true(all.equal(filterRt(inmem1, rtlim, 1),
+                          filterRt(ondisk1, rtlim, 1)))
+    expect_true(all.equal(filterRt(inmem2, rtlim, 2),
+                          filterRt(ondisk2, rtlim, 2)))
+    expect_true(all.equal(filterRt(inmem2, rtlim, 2),
+                          filterMsLevel(filterRt(ondisk, rtlim, 2), 2)))
 })
