c82c82bd20af5840375abca0f7f41f7f36e8e4ef
Laurent <lg390@cam.ac.uk>
2016-07-20 10:58:37 +0100

    Update reporter masses and add TMT10 ETD/HCD

--- R/iTRAQ5.R
+++ R/iTRAQ5.R
@@ -1,8 +1,9 @@
 iTRAQ5 <- new("ReporterIons",
-              description="4-plex iTRAQ and reporter + balance group",
-              name="iTRAQ4",
-              reporterNames=c("iTRAQ5.114","iTRAQ5.115",
-                "iTRAQ5.116","iTRAQ5.117","iTRAQ5.145"),
-              mz=c(114.1,115.1,116.1,117.1,145.1),
-              col=c("red","green","blue","yellow","grey"),
-              width=0.05)
+              description = "4-plex iTRAQ and reporter + balance group",
+              name = "iTRAQ5",
+              reporterNames = c("iTRAQ5.114", "iTRAQ5.115",
+                                "iTRAQ5.116", "iTRAQ5.117",
+                                "iTRAQ5.145"),
+              mz = c(114.1112, 115.1083, 116.1116, 117.1150, 145.1),
+              col = c("red", "green", "blue", "yellow", "grey"),
+              width = 0.05)
