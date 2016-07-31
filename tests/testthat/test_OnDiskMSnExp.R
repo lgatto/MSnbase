c21195fecc84149162b7902a77adb9e222112ac2
Laurent <lg390@cam.ac.uk>
2016-07-31 20:39:24 +0100

    use msdata files

--- tests/testthat/test_OnDiskMSnExp.R
+++ tests/testthat/test_OnDiskMSnExp.R
@@ -1,37 +1,18 @@
 context("OnDiskMSnExp class")
 
-############################################################
-## Load the required data files.
-.getMzMLFiles <- function(force.msdata = FALSE) {
-    ## Return the mzML files, the ones from the XXX package, or if run
-    ## locally, some of my test files.
-    HOST <- unlist(strsplit(system("hostname", intern = TRUE), split = ".",
-                            perl = FALSE, fixed = TRUE))[1]
-    if (HOST == "macbookjo" & !force.msdata) {
-        mzfiles <- dir("/Users/jo/R-workspaces/EURAC/2016/2016-04-21-PolarMetabolom/data/mzML/",
-                       pattern = "POS_C_O", full.names = TRUE)
-    } else {
-        require(msdata)
-        mzfiles <- c(system.file("microtofq/MM14.mzML", package = "msdata"),
-                     system.file("microtofq/MM8.mzML", package = "msdata"))
-    }
-    return(mzfiles)
-}
+library(msdata)
+mzf <- c(system.file("microtofq/MM14.mzML", package = "msdata"),
+         system.file("microtofq/MM8.mzML", package = "msdata"))
 
-mzf <- .getMzMLFiles(TRUE)[1:2]
 ## Load the data as an MSnExp into memory.
-inMem <- readMSData(files = mzf, msLevel = 1, centroided = TRUE,
-                    verbose = FALSE)
+inMem <- readMSData(files = mzf, msLevel = 1, centroided = TRUE)
 ## Load the data as OnDiskMSnExp.
-onDisk <- readMSData2(files = mzf, msLevel = 1, centroided = TRUE,
-                      verbose = FALSE)
+onDisk <- readMSData2(files = mzf, msLevel = 1, centroided = TRUE)
 
 f <- msdata::proteomics(full.names = TRUE, pattern = "TMT_Erwinia")
-multiMsInMem1 <- readMSData(files = f, msLevel = 1, centroided = TRUE,
-                            verbose = FALSE)
-multiMsInMem2 <- readMSData(files = f, msLevel = 2, centroided = TRUE,
-                            verbose = FALSE)
-multiMsOnDisk <- readMSData2(files = f, centroided = TRUE, verbose = FALSE)
+multiMsInMem1 <- readMSData(files = f, msLevel = 1, centroided = TRUE)
+multiMsInMem2 <- readMSData(files = f, msLevel = 2, centroided = TRUE)
+multiMsOnDisk <- readMSData2(files = f, centroided = TRUE)
 
 
 ############################################################
