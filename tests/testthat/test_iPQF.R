0a6ccaafde0a3158630bc9e87579b5157f4cd80d
Laurent <lg390@cam.ac.uk>
2016-10-10 23:47:40 +0100

    fix iPQF unit test to take into account new class versions

--- tests/testthat/test_iPQF.R
+++ tests/testthat/test_iPQF.R
@@ -26,6 +26,7 @@ test_that("iPQF unit test", {
                                       ratio.calc = "sum",
                                       method.combine = FALSE)
               res1@processingData <- processingData(ipqf1)
+              res1@experimentData <- experimentData(ipqf1)
               res1@.__classVersion__ <- ipqf1@.__classVersion__
               expect_true(all.equal(res1, ipqf1))
 
@@ -34,6 +35,7 @@ test_that("iPQF unit test", {
                                       redundancy.handler = "unique",
                                       fun = "iPQF")
               dflt@processingData <- processingData(ipqf1)
+              dflt@experimentData <- experimentData(ipqf1)
               res1@.__classVersion__ <- dflt@.__classVersion__
               expect_true(all.equal(res1, dflt))
               
@@ -46,6 +48,7 @@ test_that("iPQF unit test", {
                                       ratio.calc = "sum",
                                       method.combine = TRUE)
               res2@processingData <- processingData(ipqf2)
+              res2@experimentData <- experimentData(ipqf2)
               res2@.__classVersion__ <- ipqf2@.__classVersion__
               expect_true(all.equal(res2, ipqf2))
 
@@ -57,6 +60,7 @@ test_that("iPQF unit test", {
                                       ratio.calc = "sum",
                                       method.combine = FALSE)
               res3@processingData <- processingData(ipqf3)
+              res3@experimentData <- experimentData(ipqf3)
               res3@.__classVersion__ <- ipqf3@.__classVersion__
               expect_true(all.equal(res3, ipqf3))
 
@@ -68,6 +72,7 @@ test_that("iPQF unit test", {
                                       ratio.calc = "none",
                                       method.combine = FALSE)
               res4@processingData <- processingData(ipqf4)
+              res4@experimentData <- experimentData(ipqf4)
               res4@.__classVersion__ <- ipqf4@.__classVersion__
               expect_true(all.equal(res4, ipqf4))
 
@@ -79,6 +84,7 @@ test_that("iPQF unit test", {
                                       ratio.calc = "X114.ions",
                                       method.combine = FALSE)
               res5@processingData <- processingData(ipqf5)
+              res5@experimentData <- experimentData(ipqf5)
               res5@.__classVersion__ <- ipqf5@.__classVersion__
               expect_true(all.equal(res5, ipqf5))
           })
