908366974da63a63a418a97d751a8b5c0f671756
Laurent Gatto <l.gatto@dnavision.be>
2016-11-24 20:34:08 +0000

    Add bpi method (issue #168)
    o Add bpi,OnDiskMsnExp method. Parameter initial allows to specify whether the
      original data from the mzML file is returned, or whether the values are
      calculated.
    o Modify tic,OnDiskMSnExp method to always return the initial TIC from the
      mzML's header, unless parameter initial = FALSE
    o Add unit tests for both methods.
    
    From: jotsetung <johannes.rainer@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MSnbase@124448 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/torture.R
+++ inst/scripts/torture.R
@@ -1,24 +1,24 @@
 library(MSnbase)
 library(msdata)
-f <- msdata::proteomics(full.names = TRUE, pattern = "TMT_Erwinia")
+f <- msdata::proteomics(full.names = TRUE)
 
 ## readMSData
 for (i in 1:10000) {
     if (i %% 200 == 0)
         cat(i, "\n")
-    res <- readMSData(f)
-    sp <- spectra(res)
+    res <- readMSData(f[1], verbose = FALSE, msLevel. = 1)
+    res <- readMSData(f[1], verbose = FALSE, msLevel. = 2)
     sp <- res[[12]]
+    sp <- spectra(res)
 }
 
 ## readMSData2
 for (i in 1:7500) {
     if (i %% 200 == 0)
         cat(i, "\n")
-    res <- readMSData2(f, verbose = FALSE)
+    res <- readMSData2(f[1], verbose = FALSE)
+    sp <- res[[6]]
+    res <- readMSData2(f[2], verbose = FALSE)
     sp <- spectra(res)
-    sp <- res[[12]]
-    res <- readMSData2(mzf, verbose = FALSE)
-    sp <- spectra(res)
-    sp <- res[[12]]
 }
+
