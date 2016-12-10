2de341144c3d6537cc53866e3a7c236c9bb7be5e
Laurent <lg390@cam.ac.uk>
2016-12-10 15:54:30 +0000

    initial MSnExp navigation functions

---
+++ tests/testthat/test_nav.R
@@ -0,0 +1,21 @@
+context("Navigating MSnExp data")
+
+library("msdata")
+f <- proteomics(full.names = TRUE)[1]
+
+
+test_that("nextMS and prevMS", {
+    x <- readMSData2(f)
+    sp <- "X009.1"
+    expect_identical(acquisitionNum(x[[sp]]), 32926L)
+    expect_identical(acquisitionNum(nextMS(sp, x, 3L)), 32929L)
+    expect_identical(acquisitionNum(prevMS(sp, x, 3L)), 32923L)
+    expect_identical(acquisitionNum(nextMS(sp, x, 2L)), 32927L)
+    expect_identical(acquisitionNum(prevMS(sp, x, 2L)), 32925L)
+    expect_identical(acquisitionNum(nextMS(sp, x, 1L)), 32941L)
+    expect_identical(acquisitionNum(prevMS(sp, x, 1L)), 32918L)
+    expect_null(prevMS("X001.1", x, 1L))
+    expect_null(prevMS("X001.1", x, 2L))
+    expect_null(prevMS("X001.1", x, 3L))
+    expect_identical(nextMS(sp, x, 3L), nextMS(sp, x))
+})
