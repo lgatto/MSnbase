73cb30a1ae5da5ae9644ffd565f0d8d869466eec
Laurent <lg390@cam.ac.uk>
2016-12-27 21:28:25 +0000

    setting serial params on load to test travis

--- R/zzz.R
+++ R/zzz.R
@@ -12,6 +12,7 @@
 
 .onLoad <- function(libname, pkgname) {
     ## Add MSnbase options.
+    register(SerialParam())
     msOps <- list(PARALLEL_THRESH = 1000,
                   verbose = FALSE)
     options(MSnbase = msOps)
