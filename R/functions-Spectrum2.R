aa131b26d086982067ef34702b79ee2fa5ce3936
jotsetung <johannes.rainer@gmail.com>
2017-06-05 13:14:30 +0200

    Fix issue #215
    - Remove check for parameters being of length > 0.
    - Add unit tests to ensure empty spectra are correctly returned.

--- R/functions-Spectrum2.R
+++ R/functions-Spectrum2.R
@@ -94,12 +94,15 @@ Spectra2_mz_sorted <- function(peaksCount = NULL, rt = numeric(),
                                precursorCharge = NA_integer_,
                                collisionEnergy = NA, nvalues = integer()) {
     ## Argument check; make sure all is OK before calling C.
-    if (length(mz) == 0 | length(intensity) == 0 | length(nvalues) == 0) {
-        stop("Arguments 'mz', 'intensity' and 'nvalues' are required!")
-    } else {
-        if (length(mz) != length(intensity))
-            stop("Lengths of 'mz' and 'intensity' do not match!")
-    }
+    ## Fix issue #215: remove check to allow empty spectra
+    ## if (length(mz) == 0 | length(intensity) == 0 | length(nvalues) == 0) {
+    ##     stop("Arguments 'mz', 'intensity' and 'nvalues' are required!")
+    ## }
+    if (sum(nvalues) != length(mz))
+        stop("Length of 'mz' does not match with the number of values per ",
+             "spectrum")
+    if (length(mz) != length(intensity))
+        stop("Lengths of 'mz' and 'intensity' do not match!")
     nvals <- length(nvalues)
     ## Now match all of the lengths to the length of nvalues.
     if (length(peaksCount) == 0)
