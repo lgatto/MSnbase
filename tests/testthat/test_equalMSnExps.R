99303c5567d4e40c9f465617a236f3bec4d975e5
jotsetung <johannes.rainer@gmail.com>
2017-06-20 06:33:27 +0200

    Remove redundant readMSData and readMSData2 calls from unit tests

--- tests/testthat/test_equalMSnExps.R
+++ tests/testthat/test_equalMSnExps.R
@@ -1,22 +1,16 @@
 context("[OnDisk]MSnExp equality")
 
-f <- msdata::proteomics(full.names = TRUE, pattern = "TMT_Erwinia_1")
-## Set centroided to NA to fit with default behaviour of readMSData2
-inmem1 <- readMSData(f, msLevel = 1, verbose = FALSE, centroided = NA)
-inmem2 <- readMSData(f, msLevel = 2, verbose = FALSE, centroided = NA)
-ondisk <- readMSData2(f, verbose = FALSE)
-ondisk1 <- readMSData2(f, msLevel = 1, verbose = FALSE)
-ondisk2 <- readMSData2(f, msLevel = 2, verbose = FALSE)
-
 test_that("Equality function", {
     ## testing it on spectra only.
-    expect_true(all.equal(inmem1, ondisk1))
-    expect_true(all.equal(inmem2, ondisk2))
+    expect_true(all.equal(tmt_erwinia_in_mem_ms1, tmt_erwinia_on_disk_ms1))
+    expect_true(all.equal(tmt_erwinia_in_mem_ms2, tmt_erwinia_on_disk_ms2))
     ## postive controls
-    expect_true(all.equal(inmem1, inmem1))
-    expect_true(all.equal(inmem2, inmem2))
-    expect_true(all.equal(ondisk, ondisk))
+    expect_true(all.equal(tmt_erwinia_in_mem_ms1, tmt_erwinia_in_mem_ms1))
+    expect_true(all.equal(tmt_erwinia_in_mem_ms2, tmt_erwinia_in_mem_ms2))
+    expect_true(all.equal(tmt_erwinia_on_disk_ms1, tmt_erwinia_on_disk_ms1))
     ## negative controls
-    expect_false(isTRUE(all.equal(ondisk, ondisk1)))
-    expect_false(isTRUE(all.equal(ondisk, ondisk2)))
+    expect_false(isTRUE(all.equal(tmt_erwinia_in_mem_ms1,
+                                  tmt_erwinia_in_mem_ms2)))
+    expect_false(isTRUE(all.equal(tmt_erwinia_on_disk_ms1,
+                                  tmt_erwinia_on_disk_ms2)))
 })
