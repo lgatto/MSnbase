afa8b034e5830e8e196b0e94fc029dd185130030
Laurent <lg390@cam.ac.uk>
2017-08-12 21:50:14 +0100

    mention convertion to chr when reducing data.frame

--- R/utils.R
+++ R/utils.R
@@ -1226,6 +1226,11 @@ makeCamelCase <- function(x, prefix) {
 ##' combined into semicolon-separated values into a single
 ##' row/observation.
 ##'
+##' An important side-effect of reducing a `data.frame` is that all
+##' columns other than the key are converted to characters when they
+##' are collapsed to a semi-column separated value (even if only one
+##' value is present) as soon as one observation of transformed.
+##' 
 ##' @title Reduce a data.frame
 ##' @param x A \code{data.frame}.
 ##' @param key The column name (currenly only one is supported) to be
@@ -1235,10 +1240,22 @@ makeCamelCase <- function(x, prefix) {
 ##' @author Laurent Gatto
 ##' @examples
 ##' dfr <- data.frame(A = c(1, 1, 2),
-##'                   B = c("x", "y", "z"),
+##'                   B = c("x", "x", "z"),
 ##'                   C = LETTERS[1:3])
 ##' dfr
-##' reduce(dfr, key = "A")
+##' dfr2 <- reduce(dfr, key = "A")
+##' dfr2
+##' ## column A used as key is still num
+##' str(dfr2) 
+##' dfr3 <- reduce(dfr, key = "B")
+##' dfr3
+##' ## A is converted to chr; B remains factor
+##' str(dfr3)
+##' dfr4 <- data.frame(A = 1:3,
+##'                    B = LETTERS[1:3],
+##'                    C = c(TRUE, FALSE, NA))
+##' ## No effect of reducing, column classes are maintained
+##' str(reduce(dfr4, key = "B"))
 setMethod("reduce", "data.frame", 
           function(x, key, sep = ";") {
               if (nrow(x) %in% c(0, 1))
