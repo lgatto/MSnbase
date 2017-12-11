94677968b28a6cd4f4c4a2a15f6eba72c3af51a5
Arne Hindrik Smits <arne.smits@embl.de>
2017-12-11 10:28:44 +0100

    transfer to setAs

--- R/msnset2se.R
+++ R/msnset2se.R
@@ -1,63 +1,37 @@
-#' SummarizedExperiment to MSnSet object conversion
-#'
-#' \code{se2msnset} generates an MSnSet object from an SummarizedExperiment object.
-#'
-#' @param se SummarizedExperiment,
-#' Object which will be turned into an MSnSet object.
-#' @return MSnSet object.
-se2msnset <- function(se) {
-  # Check input
-  stopifnot(inherits(se, "SummarizedExperiment"))
-	
-	if(!requireNamespace("SummarizedExperiment")) {
-		stop("The SummarizedExperiment package is required for SummarizedExperiment to MSnSet conversion.")
-	}
+setAs("MSnSet",
+    "SummarizedExperiment",
+    function(from) {
+        if(!requireNamespace("SummarizedExperiment")) {
+            stop("The SummarizedExperiment package is required",
+                "for MSnSet to SummarizedExperiment conversion.")
+        }
 
-  # Extract expression, feature and pheno data
-  raw <- SummarizedExperiment::assay(se)
-  featData <- data.frame(SummarizedExperiment::rowData(se), row.names = names(se))
-  phenoData <- data.frame(SummarizedExperiment::colData(se))
+        # Extract assay, rowData and colData
+        raw <- exprs(from)
+        rowData <- fData(from)
+        colData <- pData(from)
+        
+        # Generate SE
+        SummarizedExperiment(assays = as.matrix(raw),
+            rowData = rowData,
+            colData = colData)
+})
 
-  # Generate MSnSet
-  MSnSet(exprs = as.matrix(raw),
-  			 pData = AnnotatedDataFrame(phenoData),
-  			 fData = AnnotatedDataFrame(featData))
-}
+setAs("SummarizedExperiment",
+    "MSnSet",
+    function(from) {
+        if(!requireNamespace("SummarizedExperiment")) {
+            stop("The SummarizedExperiment package is required",
+                "for SummarizedExperiment to MSnSet conversion.")
+        }
+        
+        # Extract expression, feature and pheno data
+        raw <- assay(from)
+        featData <- data.frame(rowData(from), row.names = names(from))
+        phenoData <- data.frame(colData(from))
 
-#' MSnSet to SummarizedExperiment object conversion
-#'
-#' \code{msnset2se} generates an SummarizedExperiment object from an MSnSet object.
-#'
-#' @param msnset MSnSet object,
-#' Object which will be turned into an SummarizedExperiment object.
-#' @return SummarizedExperiment object.
-msnset2se <- function(msnset) {
-  # Check input
-  stopifnot(inherits(msnset, "MSnSet"))
-
-	if(!requireNamespace("SummarizedExperiment")) {
-		stop("The SummarizedExperiment package is required for MSnSet to SummarizedExperiment conversion.")
-	}
-	
-  # Extract assay, rowData and colData
-  raw <- exprs(msnset)
-  rowData <- fData(msnset)
-  colData <- pData(msnset)
-
-  # Generate SE
-  SummarizedExperiment::SummarizedExperiment(assays = as.matrix(raw),
-  																					 rowData = rowData,
-  																					 colData = colData)
-}
-
-setAs("MSnSet", 
-			"SummarizedExperiment",
-			function(from) {
-				msnset2se(from)
-			})
-
-setAs("SummarizedExperiment", 
-			"MSnSet",
-			function(from) {
-				se2msnset(from)
-			})
+        # Generate MSnSet
+        MSnSet(exprs = as.matrix(raw),
+            pData = AnnotatedDataFrame(phenoData),
+            fData = AnnotatedDataFrame(featData))
+})
--- tests/testthat/test_msnset_se_coercions.R
+++ tests/testthat/test_msnset_se_coercions.R
@@ -1,26 +1,26 @@
 context("msnset - se conversion methods")
 
-test_that("msnset2se conversion", {
+test_that("msnset to se conversion", {
 	data(msnset, package = "MSnbase")
-	se <- msnset2se(msnset)
+	se <- as(msnset, "SummarizedExperiment")
 	
 	expect_is(se, "SummarizedExperiment")
 	expect_equal(colnames(msnset), colnames(se))
 	expect_equal(rownames(msnset), rownames(se))
-	expect_equal(Biobase::exprs(msnset), SummarizedExperiment::assay(se))
-	expect_equal(Biobase::fData(msnset), data.frame(SummarizedExperiment::rowData(se), row.names = names(se)))
-	expect_equal(Biobase::pData(msnset), data.frame(SummarizedExperiment::colData(se)))
+	expect_equal(exprs(msnset), assay(se))
+	expect_equal(fData(msnset), data.frame(rowData(se), row.names = names(se)))
+	expect_equal(pData(msnset), data.frame(colData(se)))
 })
 
-test_that("se2msnset conversion", {
+test_that("se to msnset conversion", {
 	data(msnset, package = "MSnbase")
-	se <- msnset2se(msnset)
-	msnset_back <- se2msnset(se)
+    se <- as(msnset, "SummarizedExperiment")
+	msnset_back <- as(se, "MSnSet")
 	
 	expect_is(msnset_back, "MSnSet")
 	expect_equal(colnames(msnset_back), colnames(se))
 	expect_equal(rownames(msnset_back), rownames(se))
-	expect_equal(Biobase::exprs(msnset_back), SummarizedExperiment::assay(se))
-	expect_equal(Biobase::fData(msnset_back), data.frame(SummarizedExperiment::rowData(se), row.names = names(se)))
-	expect_equal(Biobase::pData(msnset_back), data.frame(SummarizedExperiment::colData(se)))
+	expect_equal(exprs(msnset_back), assay(se))
+	expect_equal(fData(msnset_back), data.frame(rowData(se), row.names = names(se)))
+	expect_equal(pData(msnset_back), data.frame(colData(se)))
 })
