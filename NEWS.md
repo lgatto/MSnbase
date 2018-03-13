e07da71814f5eaa3686f920403c3af41f0f17cd8
Laurent <lg390@cam.ac.uk>
2018-03-13 20:26:27 +0000

    Speed up readMgfData function - see closes #319

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,8 @@
 # MSnbase 2.5
 
 ## Changes in version 2.5.8
-- New as(MSnExp, data.frame) method <2018-02-16>.
+- New as(MSnExp, data.frame) method <2018-02-16>
+- Speed up readMgfData function - see issue #319 <2018-03-13 Tue>
 
 ## Changes in version 2.5.7
 
--- tests/testthat/test_readWriteMgfData.R
+++ tests/testthat/test_readWriteMgfData.R
@@ -19,12 +19,16 @@ test_that("extractMgfSpectrum2Info", {
            mz = c(10, 11, 12, 13),
            intensity = c(100, 200, 300, 400),
            fromFile = 1L,
+           acquisitionNum = NA_integer_,
+           collisionEnergy = NA_real_,
+           precScanNum = NA_integer_,
            centroided = TRUE)
-  
+
   fdata <- c(TITLE="foobar File=\"foobar.raw\", Native ID:\"controllerType=0 controllerNumber=1, scan=100\"",
              RTINSECONDS="600",
              PEPMASS="100 50000",
              CHARGE="3+")
   result <- list(spectrum=s, fdata=fdata)
-  expect_identical(MSnbase:::extractMgfSpectrum2Info(mgf, centroided = TRUE), result)
+  expect_equal(MSnbase:::extractMgfSpectrum2Info(mgf, centroided = TRUE),
+               result)
 })
