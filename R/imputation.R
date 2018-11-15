d6b0bb41eb1119e7522f86546a3571a43666b974
Sebastian Gibb <mail@sebastiangibb.de>
2018-11-15 17:42:46 +0100

    Fix coding style in imputation.R
    Co-Authored-By: const-ae <const-ae@users.noreply.github.com>

--- R/imputation.R
+++ R/imputation.R
@@ -28,7 +28,7 @@ setMethod("impute", "MSnSet",
               if (method == "knn") {
                   imp_res <- impute.knn(exprs(object), ...)
                   .eset <- imp_res$data
-                  if(! is.null(imp_res$rng.state)){
+                  if(!is.null(imp_res$rng.state)) {
                     .Random.seed <<- imp_res$rng.state
                   }
                   exprs(object) <- .eset
