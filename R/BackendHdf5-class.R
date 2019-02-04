95926d3ed619e46a9b6cf74698f94c1b264b01cb
jotsetung <johannes.rainer@gmail.com>
2019-02-04 18:22:03 +0100

    Add .h5_read_bare and .h5_read_spectra functions (issue #416)
    - Rename slots hdf5file and md5sum to h5files and checksums (as in @sgibb's
      code).

--- R/BackendHdf5-class.R
+++ R/BackendHdf5-class.R
@@ -8,24 +8,24 @@ NULL
 #' @md
 #'
 #' @noRd
-setClass("BackendHdf5", contains = "Backend", slots = c(md5sum = "character",
-                                                        hdf5file = "character"))
+setClass("BackendHdf5", contains = "Backend", slots = c(checksums = "character",
+                                                        h5files = "character"))
 
 setMethod("show", "BackendHdf5", function(object) {
     callNextMethod()
-    cat("Hdf5 path:", unique(dirname(object@hdf5file)), "\n")
+    cat("Hdf5 path:", unique(dirname(object@h5files)), "\n")
 })
 
 #' @rdname Backend
 BackendHdf5 <- function() new("BackendHdf5")
 
-.valid.BackendHdf5.md5sum <- function(x, y) {
+.valid.BackendHdf5.checksums <- function(x, y) {
     if (length(x) != length(y))
         "different number of md5 sums and hdf5 files"
     else NULL
 }
 
-.valid.BackendHdf5.hdf5file <- function(x, y) {
+.valid.BackendHdf5.h5files <- function(x, y) {
     msg <- NULL
     if (length(x) != length(y))
         msg <- "different number of hdf5 and original files"
@@ -35,22 +35,22 @@ BackendHdf5 <- function() new("BackendHdf5")
     else NULL
 }
 
-.valid.BackendHdf5.hdf5file.exist <- function(x) {
+.valid.BackendHdf5.h5files.exist <- function(x) {
     if (!all(file.exists(x)))
         paste0("file(s) ", paste0(x[!file.exists(x)]), " not found")
     else NULL
 }
 
 setValidity("BackendHdf5", function(object) {
-    msg <- c(.valid.BackendHdf5.md5sum(object@files, object@md5sum),
-             .valid.BackendHdf5.hdf5file(object@hdf5file, object@files),
-             .valid.BackendHdf5.hdf5file.exist(object@hdf5file))
+    msg <- c(.valid.BackendHdf5.checksums(object@files, object@checksums),
+             .valid.BackendHdf5.h5files(object@h5files, object@files),
+             .valid.BackendHdf5.h5files.exist(object@h5files))
     if (length(msg)) msg
     else TRUE
 })
 
 #' Initialize the Hdf5 backend, i.e. create the hdf5 files and add their names
-#' to the @hdf5file slot.
+#' to the @h5files slot.
 #'
 #' @param object `BackendHdf5`
 #'
@@ -75,16 +75,16 @@ setMethod("backendInitialize", "BackendHdf5", function(object, files,
     dir.create(path, showWarnings = FALSE, recursive = TRUE)
     n_files <- length(files)
     object@files <- files
-    object@md5sum <- character(n_files)
-    object@hdf5file <- character(n_files)
+    object@checksums <- character(n_files)
+    object@h5files <- character(n_files)
     comp_level <- .hdf5_compression_level()
-    object@hdf5file <- file.path(path, paste0(.vdigest(files), ".h5"))
-    if (any(file.exists(object@hdf5file)))
-        stop("File(s) ", paste0(object@hdf5file[file.exists(object@hdf5file)],
+    object@h5files <- file.path(path, paste0(.vdigest(files), ".h5"))
+    if (any(file.exists(object@h5files)))
+        stop("File(s) ", paste0(object@h5files[file.exists(object@h5files)],
                                 collapse = ", "), "already exist(s). ",
              "Please choose a different 'path'.")
     for (i in seq_len(n_files)) {
-        h5 <- H5Fcreate(object@hdf5file[i])
+        h5 <- H5Fcreate(object@h5files[i])
         h5createGroup(h5, "spectra")
         h5createGroup(h5, "md5")
         H5Fclose(h5)
@@ -107,20 +107,12 @@ setMethod("backendInitialize", "BackendHdf5", function(object, files,
 setMethod("backendImportData", "BackendHdf5", function(object, spectraData,
                                                        ...,
                                                        BPPARAM = bpparam()) {
-    object@md5sum <- bpmapply(fileNames(object), object@hdf5file,
-                              FUN = .serialize_msfile_to_hdf5,
-                              BPPARAM = BPPARAM)
+    object@checksums <- bpmapply(fileNames(object), object@h5files,
+                                 FUN = .serialize_msfile_to_hdf5,
+                                 BPPARAM = BPPARAM)
     validObject(object)
     object
 })
-
-## setMethod("backendReadSpectra", "BackendHdf5", function(object, spectraData,
-##                                                         ...) {
-## })
-
-## setMethod("backendWriteSpectra", "BackendHdf5", function(object, spectra,
-##                                                          spectraData) {
-## })
 
 #' Write the content of a single mzML/etc file to an h5file. We're using the
 #' spectrum index in the file as data set ID.
@@ -148,3 +140,118 @@ setMethod("backendImportData", "BackendHdf5", function(object, spectraData,
     h5write(pks_md5, h5, paste0("md5/md5"), level = comp_level)
     pks_md5
 }
+
+## setMethod("backendReadSpectra", "BackendHdf5", function(object, spectraData,
+##                                                         ...) {
+## })
+
+#' This is based on Mike Smith's function (issue #395) that directly accesses
+#' the data without validation and file checking.
+#'
+#' @param file the ID as returned by `_H5Fopen`.
+#'
+#' @param name `character` defining the data set to be read.
+#'
+#' @return the imported data set (in most cases a `matrix`).
+#'
+#' @author Mike Smith, Johannes Rainer
+#'
+#' @md
+#'
+#' @noRd
+.h5_read_bare <- function(file, name = "") {
+    did <- .Call("_H5Dopen", file, name, NULL, PACKAGE = "rhdf5")
+    res <- .Call("_H5Dread", did, NULL, NULL, NULL, TRUE, 0L, FALSE, FALSE,
+                 PACKAGE = "rhdf5")
+    invisible(.Call("_H5Dclose", did, PACKAGE = "rhdf5"))
+    res
+}
+
+#' Read spectrum data from an hdf5 file and return a list of `Spectrum` objects.
+#'
+#' @note
+#'
+#' This function uses a constructor function that creates all `Spectrum`
+#' objects in C++ for added performance.
+#'
+#' @param fdata `data.frame` representing the feature data (`fData`) of the
+#'     spectra to be returned.
+#'
+#' @param h5file `character`: the name of the HDF5 file.
+#'
+#' @param h5md5 `character`: the md5 sum of the spectrum data in a hdf5 file.
+#'     This is checked against the hdf5 from the file and an error if thrown
+#'     if they differ, i.e. if the data was changed in the HDF5 file.
+#'
+#' @param file_name `character` with the file name of the original mzML file.
+#'
+#' @return list of `Spectrum` objects in the order of the spectra given in
+#'     param `fdata`.
+#'
+#' @author Johannes Rainer
+#'
+#' @md
+#'
+#' @noRd
+.h5_read_spectra <- function(fdata, h5file, h5md5, file_name) {
+    suppressPackageStartupMessages(require(MSnbase, quietly = TRUE))
+    fid <-.Call("_H5Fopen", h5file, 0L, PACKAGE = "rhdf5")
+    on.exit(invisible(.Call("_H5Fclose", fid, PACKAGE = "rhdf5")))
+    grp_name <- paste0(.h5_group_name(file_name), "/")
+    pks_md5 <- .h5_read_bare(fid, paste0(grp_name, "md5"))
+    if (pks_md5 != h5md5)
+        stop("The data in the Hdf5 files associated with this object appear ",
+             "to have changed! Please see the Notes section in ?Hdf5MSnExp ",
+             "for more information.")
+    mzi <- base::lapply(paste0(grp_name, fdata$spIdx),
+                        .h5_read_bare, file = fid)
+    res <- vector("list", nrow(fdata))
+    names(res) <- rownames(fdata)
+    ms1 <- which(fdata$msLevel == 1)
+    n_peaks <- base::lengths(mzi, use.names = FALSE) / 2
+    if (length(ms1)) {
+        mzi_ms1 <- do.call(rbind, mzi[ms1])
+        res[ms1] <- Spectra1_mz_sorted(
+            peaksCount = n_peaks[ms1],
+            rt = fdata$retentionTime[ms1],
+            acquisitionNum = fdata$acquisitionNum[ms1],
+            scanIndex = fdata$spIdx[ms1],
+            tic = fdata$totIonCurrent[ms1],
+            mz = mzi_ms1[, 1],
+            intensity = mzi_ms1[, 2],
+            fromFile = fdata$fileIdx[ms1],
+            centroided = fdata$centroided[ms1],
+            smoothed = fdata$smoothed[ms1],
+            polarity = fdata$polarity[ms1],
+            nvalues = n_peaks[ms1])
+    }
+    msn <- which(fdata$msLevel > 1)
+    if (length(msn)) {
+        mzi_msn <- do.call(rbind, mzi[msn])
+        res[msn] <- Spectra2_mz_sorted(
+            msLevel = fdata$msLevel[msn],
+            peaksCount = n_peaks[msn],
+            rt = fdata$retentionTime[msn],
+            acquisitionNum = fdata$acquisitionNum[msn],
+            scanIndex = fdata$spIdx[msn],
+            tic = fdata$totIonCurrent[msn],
+            mz = mzi_msn[, 1],
+            intensity = mzi_msn[, 2],
+            fromFile = fdata$fileIdx[msn],
+            centroided = fdata$centroided[msn],
+            smoothed = fdata$smoothed[msn],
+            polarity = fdata$polarity[msn],
+            merged = fdata$mergedScan[msn],
+            precScanNum = fdata$precursorScanNum[msn],
+            precursorMz = fdata$precursorMZ[msn],
+            precursorIntensity = fdata$precursorIntensity[msn],
+            precursorCharge = fdata$precursorCharge[msn],
+            collisionEnergy = fdata$collisionEnergy[msn],
+            nvalues = n_peaks[msn])
+    }
+    res
+}
+
+## setMethod("backendWriteSpectra", "BackendHdf5", function(object, spectra,
+##                                                          spectraData) {
+## })
--- tests/testthat/test_BackendHdf5.R
+++ tests/testthat/test_BackendHdf5.R
@@ -1,20 +1,20 @@
 test_that("BackendHdf5 validators work", {
-    expect_true(is.null(.valid.BackendHdf5.md5sum(5, 5)))
-    expect_true(is.character(.valid.BackendHdf5.md5sum(3, 1:2)))
-    expect_true(is.null(.valid.BackendHdf5.hdf5file(5, 5)))
-    expect_true(is.character(.valid.BackendHdf5.hdf5file(3, 1:2)))
-    expect_true(is.character(.valid.BackendHdf5.hdf5file(c(1, 1), c(1, 2))))
+    expect_true(is.null(.valid.BackendHdf5.checksums(5, 5)))
+    expect_true(is.character(.valid.BackendHdf5.checksums(3, 1:2)))
+    expect_true(is.null(.valid.BackendHdf5.h5files(5, 5)))
+    expect_true(is.character(.valid.BackendHdf5.h5files(3, 1:2)))
+    expect_true(is.character(.valid.BackendHdf5.h5files(c(1, 1), c(1, 2))))
     tmpf <- tempfile()
     write(4, tmpf)
-    expect_true(is.null(.valid.BackendHdf5.hdf5file.exist(tmpf)))
-    expect_true(is.character(.valid.BackendHdf5.hdf5file.exist("4")))
+    expect_true(is.null(.valid.BackendHdf5.h5files.exist(tmpf)))
+    expect_true(is.character(.valid.BackendHdf5.h5files.exist("4")))
 })
 
 test_that("BackendHdf5 works", {
     tst <- BackendHdf5()
     expect_true(is(tst, "BackendHdf5"))
     expect_true(validObject(tst))
-    tst@md5sum <- "r"
+    tst@checksums <- "r"
     expect_error(validObject(tst))
 })
 
@@ -22,20 +22,20 @@ test_that("backendInitialize and backendImportData,BackendHdf5 work", {
     tst <- BackendHdf5()
     dr <- tempdir()
     res <- MSnbase:::backendInitialize(tst, files = sf, path = dr)
-    expect_equal(length(res@hdf5file), 2)
-    expect_equal(length(res@md5sum), 2)
-    expect_true(all(file.exists(res@hdf5file)))
+    expect_equal(length(res@h5files), 2)
+    expect_equal(length(res@checksums), 2)
+    expect_true(all(file.exists(res@h5files)))
     expect_error(MSnbase:::backendInitialize(tst, files = sf, path = dr))
     expect_true(validObject(res))
-    expect_true(all(res@md5sum == c("", "")))
+    expect_true(all(res@checksums == c("", "")))
     show(res)
 
     ## backendImportData
     spdata <- sciex_mzr@spectraData
     res <- MSnbase:::backendImportData(res, spdata)
-    cont <- rhdf5::h5ls(res@hdf5file[1])
+    cont <- rhdf5::h5ls(res@h5files[1])
     expect_equal(sum(spdata$fileIdx == 1) + 3, nrow(cont))
-    cont <- rhdf5::h5ls(res@hdf5file[2])
+    cont <- rhdf5::h5ls(res@h5files[2])
     expect_equal(sum(spdata$fileIdx == 2) + 3, nrow(cont))
 })
 
@@ -48,4 +48,43 @@ test_that(".serialize_msfile_to_hdf5 works", {
     md5 <- MSnbase:::.serialize_msfile_to_hdf5(fileNames(sciex)[1], h5file)
     cont <- rhdf5::h5ls(h5file)
     expect_equal(nrow(cont), sum(fromFile(sciex) == 1) + 3)
+})
+
+test_that(".h5_read_bare works", {
+    ## grps <- MSnbase:::.h5_group_name(fileNames(h5_sciex))
+    ## expect_error(.h5_read_bare())
+    ## expect_error(.h5_read_bare("5"))
+    ## fid <- .Call("_H5Fopen", h5_sciex@h5files, 0L, PACKAGE = "rhdf5")
+    ## res <- MSnbase:::.h5_read_bare(fid, paste0(grps[1], "/3"))
+    ## .Call("_H5Fclose", fid, PACKAGE = "rhdf5")
+    ## res_2 <- rhdf5::h5read(h5_sciex@h5files[1],
+    ##                        name = paste0(grps[1], "/3"))
+    ## expect_equal(res, res_2)
+    ## expect_equal(res[, 1], mz(sciex[[3]]))
+    ## expect_equal(res[, 2], intensity(sciex[[3]]))
+})
+
+test_that(".h5_read_spectra works", {
+    ## fd <- fData(h5_sciex)
+    ## res_h5 <- MSnbase:::.h5_read_spectra(fd[fd$fileIdx == 1, , drop = FALSE],
+    ##                                      h5_sciex@h5files[1],
+    ##                                      h5_sciex@checksums[1],
+    ##                                      fileNames(h5_sciex)[1])
+    ## res_od <- spectra(sciex)[fromFile(sciex) == 1]
+    ## expect_equal(res_h5, res_od)
+    ## idx <- c(34, 65, 234, 453, 488)
+    ## res_h5 <- MSnbase:::.h5_read_spectra(fd[idx, ], h5_sciex@h5files[1],
+    ##                                      h5_sciex@checksums[1],
+    ##                                      fileNames(h5_sciex)[1])
+    ## expect_equal(res_h5, res_od[idx])
+    ## ## MS1 & 2 data
+    ## res_h5 <- MSnbase:::.h5_read_spectra(fData(h5_tmt), h5_tmt@h5files,
+    ##                                      h5_tmt@checksums,
+    ##                                      fileNames(h5_tmt))
+    ## res_od <- spectra(tmt_erwinia_on_disk)
+    ## expect_equal(res_h5, res_od)
+    ## expect_equal(res_od[123], MSnbase:::.h5_read_spectra(fData(h5_tmt)[123, ],
+    ##                                                      h5_tmt@h5files,
+    ##                                                      h5_tmt@checksums,
+    ##                                                      fileNames(h5_tmt)))
 })
