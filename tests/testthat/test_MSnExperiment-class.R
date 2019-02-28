93db99c4dbd56287835115319b13b4acdd21d5a9
jotsetung <johannes.rainer@gmail.com>
2019-02-28 16:01:31 +0100

    Re-add .combine_featureData unit test

--- tests/testthat/test_MSnExperiment-class.R
+++ tests/testthat/test_MSnExperiment-class.R
@@ -992,3 +992,15 @@ test_that("chromatogram,MSnExperiment works", {
     chrs_2 <- chromatogram(mzr, msLevel = 1:4)
     expect_equal(chrs, chrs_2)
 })
+
+test_that(".combine_featureData works", {
+    a <- DataFrame(col_1 = 1:5, col_2 = letters[1:5])
+    b <- DataFrame(col_3 = 5:1, col_2 = 11:15)
+    res <- .combine_featureData(a, b)
+    expect_true(ncol(res) == 3)
+    expect_equal(colnames(res), c("col_1", "col_2", "col_3"))
+    expect_equal(res$col_2, letters[1:5])
+    res <- .combine_featureData(b, a)
+    expect_equal(res$col_2, 11:15)
+    expect_error(.combine_featureData(a, b[1:4, ]), "'x' and 'y'")
+})
