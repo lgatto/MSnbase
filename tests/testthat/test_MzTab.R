a8a40280c95ec9335bd137beca982dd2b37fca51
Steffen Neumann <sneumann@ipb-halle.de>
2020-05-05 21:37:58 +0200

    Fix import of mzTab from MS-Dial

--- tests/testthat/test_MzTab.R
+++ tests/testthat/test_MzTab.R
@@ -66,6 +66,45 @@ test_that("MzTab-M creation and accessors", {
     expect_identical(length(comments(xx)), 0L)
 })
 
+test_that("MzTab-M MS-Dial import", {
+  fl <- "msdial/lcmsms_swath_lipid_height_mzTab.txt"
+  fl <- file.path(baseUrlM, fl)
+  xx <- MzTab(fl)
+  expect_true(validObject(xx))
+  expect_null(show(xx))
+  
+  ## Accessors
+  expect_is(metadata(xx), "list")
+  expect_identical(length(metadata(xx)), 178L)
+  
+  ## In mzTab-M 2.0 metadata(xx)$`mzTab-mode`=="NULL"
+  expect_identical(mzTabMode(xx), metadata(xx)$`mzTab-mode`)
+  
+  ## In mzTab-M 2.0 metadata(xx)$`mzTab-type`=="NULL"
+  expect_identical(mzTabType(xx), metadata(xx)$`mzTab-type`)
+  
+  expect_identical(fileName(xx), fl)
+  expect_is(proteins(xx), "data.frame")
+  expect_identical(dim(proteins(xx)), c(0L, 0L))
+  
+  expect_is(peptides(xx), "data.frame")
+  expect_identical(dim(peptides(xx)), c(0L, 0L))
+  
+  expect_is(psms(xx), "data.frame")
+  expect_identical(dim(psms(xx)), c(0L, 0L))
+  
+  expect_is(smallMolecules(xx), "data.frame")
+  expect_identical(dim(smallMolecules(xx)), c(1172L, 47L))
+  expect_is(moleculeFeatures(xx), "data.frame")
+  expect_identical(dim(moleculeFeatures(xx)), c(1172L, 34L))
+  expect_is(moleculeEvidence(xx), "data.frame")
+  expect_identical(dim(moleculeEvidence(xx)), c(199L, 23L))
+  
+  expect_is(comments(xx), "character")
+  expect_identical(length(comments(xx)), 1L)
+})
+
+
 test_that("MzTab reading and writing", {
     fl <- "iTRAQ_CQI.mzTab"
     fl <- file.path(baseUrl, fl)
